da3129d29d40be63af8e9ca3b4ff3d5ef484519c
Andrew Huggins <andrewhuggins@gmail.com>
2016-06-24 14:10:12 -0400

    adding the providers command which will allow the ease of installation of packages

---
+++ src/Console/Commands/AddProviders.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AHuggins\Utilities\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class AddProviders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'utils:providers';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Add the providers and aliases to the app.php file.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        
+    }
+}
